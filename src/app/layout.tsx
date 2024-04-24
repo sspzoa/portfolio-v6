dcb396d7b4d9c2e9e57eb06c147f9969a6eaa52f
Seungpyo Suh <sspyorea@gmail.com>
2024-04-24 18:04:09 +0900

    feat: details

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,27 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import type {Metadata} from "next";
+import {Inter} from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({subsets: ["latin"]});
 
 export const metadata: Metadata = {
-  title: "I'm Seungpyo Suh",
-  description: "Mobile/Frontend Developer",
+    title: "I'm Seungpyo Suh",
+    description: "Mobile/Frontend Developer",
 };
 
 export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
+                                       children,
+                                   }: Readonly<{
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  );
+    return (
+        <html lang="en">
+        <head>
+            <link rel='icon' href='/images/sspzoa_logo_round.svg'/>
+            <meta name="theme-color" content="#6D87A8"/>
+            <meta name="apple-mobile-web-app-status-bar-style" content="#6D87A8"/>
+        </head>
+        <body className={inter.className}>{children}</body>
+        </html>
+    );
 }
