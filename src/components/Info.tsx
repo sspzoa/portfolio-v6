cc1bf47922bccda66a9d1bb7a5f59a9b641fd6ab
Seungpyo Suh <sspyorea@gmail.com>
2024-05-21 22:55:13 +0900

    Update Info.tsx

--- src/components/Info.tsx
+++ src/components/Info.tsx
@@ -33,7 +33,7 @@ export default function Info() {
                     </div>
                     <div className='flex flex-col gap-[20px] items-center sm:items-start'>
                         <h1 className='text-itemTitle text-grayscale-600'>About</h1>
-                        <div className='grid grid-cols-2 sm:flex sm:flex-col gap-[20px] sm:gap-[16px]'>
+                        <div className='grid grid-cols-2 sm:flex sm:flex-col gap-[40px] sm:gap-[16px]'>
                             <div className='flex flex-col sm:flex-row w-full gap-[12px] items-center'>
                                 <h2 className='text-description text-grayscale-500 sm:w-[70px]'>2024.03~</h2>
                                 <div className='flex flex-col gap-[2px]'>
