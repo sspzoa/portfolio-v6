55bd19cea27352286dbf13620c6ace4c4b47772b
Seungpyo Suh <sspyorea@gmail.com>
2024-05-26 00:04:58 +0900

    feat: add intro

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,20 @@
 import Info from "@/components/Info";
 import Awards from "@/components/Awards";
 import Projects from "@/components/Projects";
+import Portfolio from "@/components/Portfolio";
+import {Wing} from "@/components/svg";
+import Image from "next/image";
 
 export default function Home() {
     const currentYear = new Date().getFullYear();
     return (
         <>
-            <div className='flex flex-col gap-[10px] sm:flex-row sm:gap-0'>
-                <div className='fixed left-0 hidden sm:block z-20'>
-                    <Info/>
+            <div>
+                <div className='flex justify-center items-center w-full h-[100vh] flex-col gap-[32px]'>
+                    <Image className='shadow-2xl shadow-primary-brand rounded-[32px]' src={'/images/sspzoa_logo.svg'} alt={'sspzoa'} width={180} height={180}/>
+                    <h1 className='text-[32px] sm:text-[48px] font-[800] text-grayscale-1000'>I&apos;m Seungpyo Suh</h1>
                 </div>
-                <div className='flex-shrink-0 sm:opacity-0 opacity-100 z-10'>
-                    <Info/>
-                </div>
-                <div className='flex flex-col w-full px-[16px] py-[30px] gap-[30px] sm:gap-[50px] sm:p-[50px]'>
-                    <Awards/>
-                    <Projects/>
-                    <div>
-                        <h1 className='text-center sm:text-right text-readable text-grayscale-500'>Copyright © 2023-{currentYear} Seungpyo Suh All rights reserved.</h1>
-                    </div>
-                </div>
+                <Portfolio/>
             </div>
         </>
     );
--- src/components/Info.tsx
+++ src/components/Info.tsx
@@ -9,7 +9,7 @@ export default function Info() {
     return (
         <>
             <div
-                className='flex-shrink-0 h-auto w-full sm:w-[400px] sm:h-[100vh] px-[30px] py-[50px] sm:p-[50px] flex flex-col justify-between gap-[40px] bg-grayscale-100'>
+                className='flex-shrink-0 h-auto w-full sm:w-[350px] sm:h-[100vh] px-[30px] py-[50px] sm:p-[50px] flex flex-col justify-between gap-[40px]'>
                 <div className='flex flex-col gap-[50px]'>
                     <div className='flex flex-col gap-[10px] items-center sm:items-start'>
                         <Image className='rounded-full' src={'/images/sspzoa_logo.svg'} alt='sspzoa' width={120}
