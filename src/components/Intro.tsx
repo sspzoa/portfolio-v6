b2f35fd3bccf1c457ba45818bfa2c97f0683976f
Seungpyo Suh <sspyorea@gmail.com>
2024-07-19 22:54:43 +0900

    Update Intro.tsx

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -1,10 +1,12 @@
 'use client'
 
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import Image from "next/image";
 import {Arrow} from "@/components/svg";
 
 const Intro: React.FC = () => {
+    const [clickCount, setClickCount] = useState(0);
+
     useEffect(() => {
         const handleScroll = (e: MouseEvent) => {
             e.preventDefault();
@@ -26,15 +28,26 @@ const Intro: React.FC = () => {
         };
     }, []);
 
+    const handleImageClick = () => {
+        setClickCount(prevCount => {
+            if (prevCount === 4) {
+                window.open('https://pop-one-bay.vercel.app/', '_blank');
+                return 0;
+            }
+            return prevCount + 1;
+        });
+    };
+
     return (
         <>
             <div className='flex justify-center items-center w-full h-[100vh] flex-col gap-[32px]'>
                 <Image
-                    className='shadow-2xl shadow-primary-brand rounded-[32px] ease-in-out duration-300'
+                    className='shadow-2xl shadow-primary-brand rounded-[32px] ease-in-out duration-300 cursor-pointer'
                     src={'/images/sspzoa_logo.svg'}
                     alt={'sspzoa'}
                     width={180}
                     height={180}
+                    onClick={handleImageClick}
                 />
                 <h1 className='text-[32px] sm:text-[48px] font-[800] text-grayscale-1000'>I&apos;m Seungpyo Suh</h1>
                 <a id="scroll-link" href="#"
